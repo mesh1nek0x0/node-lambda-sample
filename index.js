b3c496e379a1bbcf61be31880a2c758ef350d421
mesh1neko <mesh1nek0x0@gmail.com>
2017-07-23 16:23:44 +0900

    fix phantom instance exit in finally

--- index.js
+++ index.js
@@ -6,25 +6,36 @@ const phantom = require('phantom');
 
 module.exports.handler = function (event, context) {
     console.log('lambda is started');
+    let instance;
 
     Promise.coroutine(function* () {
-        const instance = yield phantom.create();
+        instance = yield phantom.create();
         const page = yield instance.createPage();
 
         let status = yield page.open('https://httpbin.org/');
         console.log(status);
+        if (status != 'success') {
+            throw new Error('page is not opend');
+        }
 
         yield page.evaluate(function() {
             return document.getElementsByTagName('title')[0].innerHTML;
         }).then(function(title){
             console.log(title);
         });
-
-        yield instance.exit();
+    })().then(() => {
         console.log('lambda will ended with success');
-    })().catch(() => {
+        context.succeed('done success');
+    }).catch((err) => {
+        console.error(err.stack);
         console.log('lambda will ended with failure');
+        context.fail('done failure');
     }).finally(() => {
-        context.succeed('lambda done');
+        console.log('finally is started');
+        Promise.coroutine(function *() {
+            yield instance.exit();
+        })().then(() => {
+            console.log('lambda is closed');
+        });
     });
 };
